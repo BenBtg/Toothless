52a7dcfde5f18c40a2968c2fd25698c23e1240ef
Ben Butttigieg <ben.buttigieg@microsoft.com>
2020-04-15 16:25:44 +0100

    Added swift sample

---
+++ Swift/ToothlessSwift/AppDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.swift
+//  ToothlessSwift
+//
+//  Created by Ben Buttigieg on 15/04/2020.
+//  Copyright © 2020 Ben Buttigieg. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var centralManager: CBCentralManager!
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+       centralManager = CBCentralManager(delegate: self, queue: nil)
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+extension AppDelegate: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+          case .unknown:
+            print("central.state is .unknown")
+          case .resetting:
+            print("central.state is .resetting")
+          case .unsupported:
+            print("central.state is .unsupported")
+          case .unauthorized:
+            print("central.state is .unauthorized")
+          case .poweredOff:
+            print("central.state is .poweredOff")
+          case .poweredOn:
+            print("central.state is .poweredOn")
+        }
+    }
+    
+
+}
+
+
---
+++ Swift/ToothlessSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToothlessSwift
+//
+//  Created by Ben Buttigieg on 15/04/2020.
+//  Copyright © 2020 Ben Buttigieg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
